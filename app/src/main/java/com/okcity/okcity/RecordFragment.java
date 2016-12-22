fd496472dafd29cbcbfeffa51b4476fbfef6aff2
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-12-21 16:32:28 -0800

    Initial commit

---
+++ app/src/main/java/com/okcity/okcity/RecordFragment.java
@@ -0,0 +1,33 @@
+package com.okcity.okcity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecordFragment extends Fragment {
+
+    private MapView mapView;
+
+    public RecordFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_record, container, false);
+
+        mapView
+
+        return v;
+    }
+
+}
