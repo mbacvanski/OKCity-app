daf515289a13760d43a2f941aefc18337e9d2459
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-12-22 23:12:18 -0800

    Add map

--- app/src/main/java/com/okcity/okcity/RecordFragment.java
+++ app/src/main/java/com/okcity/okcity/RecordFragment.java
@@ -1,33 +1,131 @@
 package com.okcity.okcity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
+
 public class RecordFragment extends Fragment {
 
-    private MapView mapView;
+    MapView mMapView;
+    private GoogleMap googleMap;
+    private final int FINE_LOCATION_PERMISSION = 0;
+    private final int COARSE_LOCATION_PERMISSION = 1;
 
-    public RecordFragment() {
-        // Required empty public constructor
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_record, container, false);
+
+//        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .enableAutoManage(this /* FragmentActivity */,
+//                        this /* OnConnectionFailedListener */)
+//                .addApi(.API)
+//                .addScope(Drive.SCOPE_FILE)
+//                .build();
+
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+                boolean fineLocation = checkPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+                boolean coarseLocation = checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
+                if (!fineLocation) {
+                    askForPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
+                            COARSE_LOCATION_PERMISSION);
+                }
+                if (!coarseLocation) {
+                    askForPermissions(Manifest.permission.ACCESS_FINE_LOCATION,
+                            FINE_LOCATION_PERMISSION);
+                }
+                if (fineLocation && coarseLocation) {
+                    // We have all the permissions we need
+                    moveMarkerToUserPosition(googleMap);
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    private void askForPermissions(String permission, int requestCode) {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, requestCode);
+    }
+
+    private boolean checkPermission(String permission) {
+        return ContextCompat.checkSelfPermission(getActivity(), permission)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void moveMarkerToUserPosition(final GoogleMap googleMap) {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            googleMap.setMyLocationEnabled(true);
+
+//            Location currentLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        }
+
+        // For dropping a marker at a point on the Map
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+        // For zooming automatically to the location of the marker
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_record, container, false);
-
-        mapView
-
-        return v;
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
 }
